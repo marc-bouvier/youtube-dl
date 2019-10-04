5fb6229c59e6c2c4057999704f7895e92cf903ef
nunurs89 <30862104+nunurs89@users.noreply.github.com>
2019-10-04 10:26:12 +0200

    Update navbar.php

--- app/dep/navbar.php
+++ app/dep/navbar.php
@@ -1,5 +1,5 @@
 <!-- Fixed navbar -->
-<?php include_once("CONFIG.php"); include_once("nav_links");?>
+<?php include_once("CONFIG.php"); include_once("nav_links.php");?>
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">

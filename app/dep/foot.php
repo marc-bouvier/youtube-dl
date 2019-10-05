6865084dc5ebf49a56e9667936474c819656c86f
nunurs89 <30862104+nunurs89@users.noreply.github.com>
2019-10-05 14:49:56 +0200

    Update foot.php

--- app/dep/foot.php
+++ app/dep/foot.php
@@ -3,6 +3,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo ROOT; ?>./js/jquery.fancybox.pack.js"></script>
+    <script type="text/javascript" src="<?php echo ROOT; ?>js/jquery.fancybox.pack.js"></script>
   </body>
 </html>

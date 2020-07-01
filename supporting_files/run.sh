bfd4ada0fae24af74676070a2ff3f9e219993e42
nunurs89 <30862104+nunurs89@users.noreply.github.com>
2020-07-01 11:49:19 +0200

    Update run.sh

--- supporting_files/run.sh
+++ supporting_files/run.sh
@@ -22,7 +22,7 @@ then
         -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/5.6/apache2/php.ini
 else
     sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-        -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.2/apache2/php.ini
+        -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.4/apache2/php.ini
 fi
 
 
